7f5ba76e2de92640b150fb940e0f9cf8bd33db13
Hashir <mail2hashirpm@gmail.com>
2022-06-15 21:29:10 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         > 
-          secureX
+          SECUREX
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
